814f1f483de9fd5febce11cda3922b3b0c2ffcd3
Emmanuel Chinyeotito <chinyeotitoemmanuel@gmail.com>
2025-12-24 09:48:39 +0000

    mets les ecris en blancs (_for element button_)

--- src/components/pages/home/creator-promo.tsx
+++ src/components/pages/home/creator-promo.tsx
@@ -13,7 +13,7 @@ export function CreatorPromo() {
         </p>
       </div>
       <div className="relative z-10 shrink-0">
-        <Button size="lg" variant="secondary" className="bg-primary-foreground text-background hover:bg-primary-foreground/90 shadow-lg">
+        <Button size="lg" variant="secondary" className="bg-primary-foreground text-primary hover:bg-primary-foreground/90 shadow-lg">
           Start Creating
         </Button>
       </div>
